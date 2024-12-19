a121375ffbc9e1f018703f697d549d3e72519095
JinA <JinA@172.16.106.178>
2024-12-19 11:09:08 +0900

    번호자동부여

---
+++ hospital/src/main/java/hospital/command/WardPsCommand.java
@@ -0,0 +1,23 @@
+package hospital.command;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class WardPsCommand {
+	
+	String wardPsNum;
+	String hospNum;
+	String empNum;
+	String diagName;
+	String diagCont;
+	String medicPres;
+	String examPres;
+	String hanPres;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	Date inputDate;
+}
---
+++ hospital/src/main/java/hospital/service/AutoNumService.java
@@ -0,0 +1,25 @@
+package hospital.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import hospital.mapper.AutoMapper;
+
+@Service
+public class AutoNumService {
+	
+	@Autowired
+	AutoMapper autoMapper;
+	
+	public String execute(String sep, int len
+						, String col_name, String table_name) {
+		/// sep은 번호에 들어갈 단어 ex) emp_000 -> emp
+		/// len은 sep의 총글자 수 + 1 ex) emp_ = 4글자 -> len = 4 + 1 = 5
+		/// col_name은 컬럼명, table_name은 테이블명
+		
+		String autoNum
+		= autoMapper.selectNum(sep, len, col_name, table_name);
+		return autoNum;
+	}
+
+}
