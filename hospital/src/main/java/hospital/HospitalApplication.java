98f5e06f4ad39a0488336e7bc81d94686655ed34
YangJinSeo <wls00tj0827@gmail.com>
2025-01-03 16:50:14 +0900

    css 임시 적용

--- hospital/src/main/java/hospital/HospitalApplication.java
+++ hospital/src/main/java/hospital/HospitalApplication.java
@@ -3,7 +3,6 @@ package hospital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
@@ -23,6 +22,28 @@ public class HospitalApplication {
 		
 		return "thymeleaf/empIndex";
 	}
+	@RequestMapping("404")
+	public String error() {
+		
+		return "thymeleaf/404";
+	}
+	@RequestMapping("blog-single")
+	public String blog() {
+		
+		return "thymeleaf/blog-single";
+	}
+	@RequestMapping("contact")
+	public String contact() {
+		
+		return "thymeleaf/contact";
+	}
+	@RequestMapping("appointment")
+	public String appointment() {
+		
+		return "thymeleaf/appointment";
+	}
+	
+	
 	
 
 }
