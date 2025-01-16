78b9c098856ffacf1073c54772583992bff189a2
jkl9977 <127465004+jkl9977@users.noreply.github.com>
2025-01-16 19:00:05 +0900

    info 컨텐츠 일부 추가 / 로그인 옆 사람 이모지 추가

--- hospital/src/main/java/hospital/controller/InformationController.java
+++ hospital/src/main/java/hospital/controller/InformationController.java
@@ -1,17 +1,32 @@
 package hospital.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import hospital.service.info.SectionService;
 
 @Controller
 @RequestMapping("info")
 public class InformationController {
 	
-	@GetMapping("directions") //오시는 길
+	@GetMapping("directions") //오시는 길(지도 넣기)
 	public String directions() {
 		return "thymeleaf/info/directions";
 	}
 	
+	@GetMapping("section") //진료과 화면
+	public String section() {
+		return "thymeleaf/info/section";
+	}
+	
+	@Autowired
+	SectionService sectionService;
+	@GetMapping("sections")
+	public String section(Integer num) {
+		sectionService.execute(num);
+		return "thymeleaf/info/sections";
+	}
 
 }
---
+++ hospital/src/main/java/hospital/service/info/SectionService.java
@@ -0,0 +1,35 @@
+package hospital.service.info;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SectionService {
+
+	public void execute(Integer num) {
+		if(num!=null) {
+			if(num==1) {
+				
+			}else if(num==2) {
+				
+			}else if(num==3) {
+				
+			}else if(num==4) {
+				
+			}else if(num==5) {
+				
+			}else if(num==6) {
+				
+			}else if(num==7) {
+				
+			}else if(num==8) {
+				
+			}else if(num==9) {
+				
+			}else if(num==10) {
+				
+			}
+		}
+		
+	}
+
+}
